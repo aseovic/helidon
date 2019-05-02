76b52b6d316f66d4d8b730dd7b3274e1a6f1af2c
Jonathan Knight <jk@thegridman.com>
2019-05-02 15:21:48 +0300

    Clean-up after rebasing on master

--- examples/grpc/microprofile/basic-server-implicit/src/main/java/io/helidon/microprofile/grpc/example/basic/implicit/AdditionalServices.java
+++ examples/grpc/microprofile/basic-server-implicit/src/main/java/io/helidon/microprofile/grpc/example/basic/implicit/AdditionalServices.java
@@ -16,72 +16,38 @@
 
 package io.helidon.microprofile.grpc.example.basic.implicit;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.AfterBeanDiscovery;
-import javax.enterprise.inject.spi.Extension;
-
 import io.helidon.config.Config;
 import io.helidon.grpc.examples.common.EchoService;
 import io.helidon.grpc.examples.common.GreetService;
-import io.helidon.grpc.server.GrpcService;
-
-import io.grpc.BindableService;
+import io.helidon.microprofile.grpc.server.spi.GrpcMpContext;
+import io.helidon.microprofile.grpc.server.spi.GrpcMpExtension;
 
 /**
  * An example of adding additional non-managed bean gRPC services.
  * <p>
- * This class is a CDI {@link Extension} that will receive an
- * {@link AfterBeanDiscovery} event that it will then use to add
- * additional services as managed CDI beans.
+ * This class is a {@link GrpcMpExtension} that will be called by
+ * the  {@link io.helidon.microprofile.grpc.server.GrpcServerCdiExtension
+ * gRPC MP Extension} prior to starting the gRPC server.
  * <p>
- * As a CDI extension this class also needs to be specified in the
- * {@code META-INF/services/javax.enterprise.inject.spi.Extension}
+ * As an extension this class also needs to be specified in the
+ * {@code META-INF/services/io.helidon.microprofile.grpc.server.spi.GrpcMpExtension}
  * file (or for Java 9+ modules in the {@code module-info.java} file).
  */
 public class AdditionalServices
-        implements Extension {
+        implements GrpcMpExtension {
     /**
      * Add additional gRPC services as managed beans.
      * <p>
      * These are classes that are on the classpath but for whatever reason are not
      * annotated as managed beans (for example we do not own the source) but we want
      * them to be located and loaded by the server.
-     * <p>
-     * We can add each service we want as a new bean with a type of either
-     * {@link io.helidon.grpc.server.GrpcService} or {@link io.grpc.BindableService}
-     * and a scope of {@link javax.enterprise.context.ApplicationScoped} so that is
-     * is discovered when the MP gRPC server starts.
      *
-     * @param event  the {@link AfterBeanDiscovery} event injected by CDI
+     * @param context  the {@link GrpcMpContext} to use to add the extra services
      */
-    public void afterBean(final @Observes AfterBeanDiscovery event) {
-        // Add the GreetService as a managed bean - GreetService is a POJO service implementation
-        event.addBean()
-                .beanClass(GreetService.class)
-                // Specify the constructor to use
-                .createWith(e -> new GreetService(Config.empty()))
-                // Make the GreetService an application scoped singleton
-                .scope(ApplicationScoped.class)
-                // Specify the types that may be used for discovery,
-                // this must include GrpcService.class as this class
-                // is used by the gRPC MP server to discover services
-                .types(GrpcService.class, GreetService.class)
-                // specify a unique ID for the bean
-                .id(GreetService.class.getSimpleName());
-
-        // Add the EchoService as a managed bean - EchoService is a protocol buffer generated service implementation
-        event.addBean()
-                .beanClass(EchoService.class)
-                // Specify the constructor to use to create an
-                .createWith(e -> new EchoService())
-                // Make the EchoService an application scoped singleton
-                .scope(ApplicationScoped.class)
-                // Specify the types that may be used for discovery,
-                // this must include BindableService.class as this class
-                // is used by the gRPC MP server to discover services
-                .types(BindableService.class, EchoService.class)
-                // specify a unique ID for the bean
-                .id(EchoService.class.getSimpleName());
+    @Override
+    public void configure(GrpcMpContext context) {
+        context.routing()
+               .register(new GreetService(Config.empty()))
+               .register(new EchoService());
     }
 }
